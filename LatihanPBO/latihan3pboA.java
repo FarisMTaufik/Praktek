d61d28a61d1c5eac7913019f2bfa75d05ecc6dca
FarisMTaufik <161101497+FarisMTaufik@users.noreply.github.com>
2024-07-10 14:54:10 +0700

    Add files via upload

---
+++ LatihanPBO/latihan3pboA.java
@@ -0,0 +1,17 @@
+package LatihanPBO;
+
+
+/*
+
+nama : Faris Muhammad Taufik
+no nim : 2301092015
+soal no 3 for loop
+*/
+public class latihan3pboA {
+    public static void main(String[] args) {
+        for ( int i = 0; i <= 100;i++)
+        {
+            System.out.println("Andrean Willian Syach");
+        }
+    }
+}
